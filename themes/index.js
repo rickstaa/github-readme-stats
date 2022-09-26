3a52cd499a46bb243554ac1e48570e943666695f
rickstaa <rick.staa@outlook.com>
2022-09-26 17:08:20 +0200

    update naming

--- themes/index.js
+++ themes/index.js
@@ -361,7 +361,7 @@ export const themes = {
     text_color: "e0def4",
     bg_color: "191724",
   },
-  rose_pine: {
+  rose_pine2: {
     title_color: "9ccfd8",
     icon_color: "ebbcba",
     text_color: "e0def4",
